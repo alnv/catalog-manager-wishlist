9f7d7e0cf989c287151f357105419214768a25f7
Alexander Naumov <me@alexandernaumov.de>
2018-01-04 12:47:14 +0100

    add hook

--- config/config.php
+++ config/config.php
@@ -1,4 +1,5 @@
 <?php
 
+$GLOBALS['TL_HOOKS']['catalogManagerViewQuery'][] = [ 'CMWishlist\CatalogViewWishlist', 'setQuery' ];
 $GLOBALS['TL_HOOKS']['catalogManagerInitializeView'][] = [ 'CMWishlist\CatalogViewWishlist', 'initialize' ];
 $GLOBALS['TL_HOOKS']['catalogManagerRenderCatalog'][] = [ 'CMWishlist\CatalogViewWishlist', 'renderCatalog' ];
