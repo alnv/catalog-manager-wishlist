d922afac63cca95808fcc7f8af39f354a106206a
Alexander Naumov <me@alexandernaumov.de>
2018-01-04 12:46:30 +0100

    add labels

---
+++ languages/de/tl_module.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_module']['wishlistAmount'] = [ 'Anzahl-Eingabefeld', 'Hier können Sie ein Eingabefeld für die Anzahl einbinden.' ];
+$GLOBALS['TL_LANG']['tl_module']['wishlistWidget'] = [ 'Wunschliste verwenden', 'Hier können Sie die Daten in eine Wunschliste eintragen.' ];
+$GLOBALS['TL_LANG']['tl_module']['wishlistDisableRemoveButton'] = [ 'Entfernen-Button deaktivieren', 'Hier können Sie den entfernen Button deaktivieren.' ];
