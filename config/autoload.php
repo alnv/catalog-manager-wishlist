e933c3197cdf108cfa4c2faa9b9a1fd94be63c56
Alexander Naumov <me@alexandernaumov.de>
2018-01-04 14:56:26 +0100

    add wishlist template

--- config/autoload.php
+++ config/autoload.php
@@ -4,11 +4,14 @@ ClassLoader::addNamespace( 'CMWishlist' );
 
 ClassLoader::addClasses([
 
-    'CMWishlist\CatalogViewWishlist' => 'system/modules/catalog-manager-wishlist/CatalogViewWishlist.php'
+    'CMWishlist\WishlistView' => 'system/modules/catalog-manager-wishlist/WishlistView.php',
+    'CMWishlist\WishlistModule' => 'system/modules/catalog-manager-wishlist/WishlistModule.php',
+    'CMWishlist\WishlistInserttag' => 'system/modules/catalog-manager-wishlist/WishlistInserttag.php'
 ]);
 
 TemplateLoader::addFiles([
 
+    'wishlist_view' => 'system/modules/catalog-manager-wishlist/templates',
     'ctlg_view_table' => 'system/modules/catalog-manager-wishlist/templates',
     'ctlg_view_teaser' => 'system/modules/catalog-manager-wishlist/templates',
     'mod_catalog_table' => 'system/modules/catalog-manager-wishlist/templates',
