4eb5a99b0b1249cf91e8642b611e604d767d3c77
Alexander Naumov <me@alexandernaumov.de>
2018-01-04 14:54:45 +0100

    add wishlist inserttag

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
-$GLOBALS['TL_HOOKS']['catalogManagerViewQuery'][] = [ 'CMWishlist\CatalogViewWishlist', 'setQuery' ];
-$GLOBALS['TL_HOOKS']['catalogManagerInitializeView'][] = [ 'CMWishlist\CatalogViewWishlist', 'initialize' ];
-$GLOBALS['TL_HOOKS']['catalogManagerRenderCatalog'][] = [ 'CMWishlist\CatalogViewWishlist', 'renderCatalog' ];
+$GLOBALS['TL_HOOKS']['catalogManagerViewQuery'][] = [ 'CMWishlist\WishlistModule', 'setQuery' ];
+$GLOBALS['TL_HOOKS']['catalogManagerInitializeView'][] = [ 'CMWishlist\WishlistModule', 'initialize' ];
+$GLOBALS['TL_HOOKS']['catalogManagerRenderCatalog'][] = [ 'CMWishlist\WishlistModule', 'renderCatalog' ];
+
+$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = [ 'CMWishlist\WishlistInserttag', 'getInsertTagValue' ];
