1badc71565d1ac14a2077b0ae4d3b31f7f7d42f7
Alexander Naumov <me@alexandernaumov.de>
2018-01-04 12:47:33 +0100

    add enable filter field

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -1,16 +1,60 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['catalogUniversalView'] = str_replace( 'catalogUseSocialSharingButtons;', 'catalogUseSocialSharingButtons;{wishlist_legend},catalogUseWishlist;', $GLOBALS['TL_DCA']['tl_module']['palettes']['catalogUniversalView'] );
+$GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][] = 'wishlistWidget';
+$GLOBALS['TL_DCA']['tl_module']['subpalettes']['wishlistWidget'] = 'wishlistEnableFilter,wishlistAmount,wishlistDisableRemoveButton';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['catalogUniversalView'] = str_replace( 'catalogUseSocialSharingButtons;', 'catalogUseSocialSharingButtons;{wishlist_legend},wishlistWidget;', $GLOBALS['TL_DCA']['tl_module']['palettes']['catalogUniversalView'] );
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['catalogUseWishlist'] = [
+$GLOBALS['TL_DCA']['tl_module']['fields']['wishlistWidget'] = [
 
-    'label' => &$GLOBALS['TL_LANG']['tl_module']['catalogUseWishlist'],
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['wishlistWidget'],
     'inputType' => 'checkbox',
 
     'eval' => [
 
-        'tl_class' => 'clr',
-        'submitOnChange' => true,
+        'tl_class' => 'w50',
+        'submitOnChange' => true
+    ],
+
+    'exclude' => true,
+    'sql' => "char(1) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['wishlistAmount'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['wishlistAmount'],
+    'inputType' => 'checkbox',
+
+    'eval' => [
+
+        'tl_class' => 'w50'
+    ],
+
+    'exclude' => true,
+    'sql' => "char(1) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['wishlistDisableRemoveButton'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['wishlistDisableRemoveButton'],
+    'inputType' => 'checkbox',
+
+    'eval' => [
+
+        'tl_class' => 'w50'
+    ],
+
+    'exclude' => true,
+    'sql' => "char(1) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['wishlistEnableFilter'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['wishlistEnableFilter'],
+    'inputType' => 'checkbox',
+
+    'eval' => [
+
+        'tl_class' => 'w50'
     ],
 
     'exclude' => true,
