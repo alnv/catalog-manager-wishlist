e8effa4d852a206544029eb0bb0ff3276e7716de
Alexander Naumov <me@alexandernaumov.de>
2018-01-05 13:36:46 +0100

    add english labels

---
+++ languages/en/tl_module.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_module']['wishlist_legend'] = 'Wishlist settings';
+
+$GLOBALS['TL_LANG']['tl_module']['wishlistEnableFilter'] = [ 'Filter', 'Only user-selected entries are displayed.' ];
+$GLOBALS['TL_LANG']['tl_module']['wishlistAmount'] = [ 'Use amount', 'Users can add a certain amount to the wishlist.' ];
+$GLOBALS['TL_LANG']['tl_module']['wishlistWidget'] = [ 'Use wishlist', 'Users can add entries to your personal wish list.' ];
+$GLOBALS['TL_LANG']['tl_module']['wishlistDisableRemoveButton'] = [ 'Disable remove button', 'Here you can deactivate the "remove from wishlist" button.' ];
