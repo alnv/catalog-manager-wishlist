ed87a31ab63c63ab2aa53fd9d145641f25cbaeaf
Alexander Naumov <me@alexandernaumov.de>
2018-01-05 13:38:43 +0100

    add english labels

---
+++ languages/en/default.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['wishlist'] = 'Wishlist';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['wishlistAmount'] = 'Amount';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['wishlistAddButton'] = 'Add to Wishlist';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['wishlistUpdateButton'] = 'Change amount';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['wishlistDeleteButton'] = 'Remove from wish list';
